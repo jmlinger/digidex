8a0f29736d42d7e872b3f6bef536cd690937fba7
jmlinger <jmlinger10@gmail.com>
2023-12-12 17:26:22 -0300

    refactor(server): modularize server creation and startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,45 @@
+// server.ts
+
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-
 import { digimonRoutes } from './routes/digimonRoutes'
 import { connectToDatabase } from './models/db'
 
-const app = fastify()
+export async function createServer() {
+  const app = fastify()
+
+  app.register(cors, {
+    origin: true,
+  })
+
+  connectToDatabase()
+
+  app.register(digimonRoutes)
+
+  await app.ready()
+
+  return app
+}
 
 const serverOptions = {
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }
 
-app.register(cors, {
-  origin: true,
-})
-
-connectToDatabase()
-
-app.register(digimonRoutes)
-
-app
-  .listen(serverOptions)
-  .then(() => {
-    console.log(
-      `HTTP Server Running on ${serverOptions.host}:${serverOptions.port}`,
-    )
-  })
-  .catch((err) => {
-    if (err) {
+if (require.main === module) {
+  const startServer = async () => {
+    try {
+      const fastifyInstance = await createServer()
+      await fastifyInstance.listen(serverOptions)
+      console.log(
+        `HTTP Server Running on ${serverOptions.host}:${serverOptions.port}`,
+      )
+    } catch (err) {
       console.error('Error when starting the server:', err)
       process.exit(1)
     }
-  })
+  }
+
+  startServer()
+}
