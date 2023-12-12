30fe8fb1db9aca208009e12abc843886272203c1
jmlinger <jmlinger10@gmail.com>
2023-12-11 21:17:27 -0300

    feat(upButton): add up button component

---
+++ web/src/app/components/upButton.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import Button from './button'
+import { ArrowUp } from 'lucide-react'
+
+export default function UpButton() {
+  const [visible, setVisible] = useState(false)
+
+  function scrolltoTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  useEffect(() => {
+    const onScroll = () => {
+      window.scrollY >= 720 && setVisible(true)
+      window.scrollY < 720 && setVisible(false)
+    }
+    window.removeEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll, { passive: true })
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  return (
+    <div
+      data-visible={visible}
+      className="fixed bottom-2 right-2 z-10 scale-0 rounded-full bg-newblue-950 transition-transform duration-150 data-[visible=true]:scale-100"
+    >
+      <Button className="rounded-full font-semibold" onClick={scrolltoTop}>
+        <ArrowUp className="h-7 w-7 sm:h-10 sm:w-10" />
+      </Button>
+    </div>
+  )
+}
