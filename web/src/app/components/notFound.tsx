7197e9e3c69f9b7e48422d710bd0abcca1fce94a
jmlinger <jmlinger10@gmail.com>
2023-12-11 21:27:31 -0300

    feat(notFound): add not found component

---
+++ web/src/app/components/notFound.tsx
@@ -0,0 +1,17 @@
+import agumon from '../assets/newlogo.png'
+import Image from 'next/image'
+
+export default function NotFound() {
+  return (
+    <div className="absolute left-1/2 top-[50%] flex -translate-x-1/2 -translate-y-1/2 flex-wrap items-center justify-center">
+      <Image
+        src={agumon}
+        alt="image"
+        className="max-w-[120px] sm:max-w-[240px]"
+      />
+      <h1 className="relative whitespace-nowrap text-xl font-bold text-newYellow-500 sm:text-4xl xl:text-6xl">
+        DIGIMON NOT FOUND
+      </h1>
+    </div>
+  )
+}
