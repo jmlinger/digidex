39e59494b96537d928d8ee00c685b7266809f9c6
jmlinger <jmlinger10@gmail.com>
2023-12-11 21:22:27 -0300

    feat(api): add function to fetch Digimons with filters

---
+++ web/src/app/services/digimonApi.ts
@@ -0,0 +1,27 @@
+import { api } from '../services/api'
+
+export async function getDigimons(context: any) {
+  const { digimonLevel, digimonName, setDigimonName, setDigimonLevel } = context
+
+  if (digimonName || digimonLevel) {
+    try {
+      const { data } = await api.get(
+        `/digimon?name=${digimonName}&level=${digimonLevel}`,
+      )
+
+      setDigimonName('')
+      setDigimonLevel('')
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  try {
+    const { data } = await api.get(`/digimon?name=&level=`)
+
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
